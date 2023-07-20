04bcc5116a8c187a271391599bb7f6e5df405c5b
MouaadLaouar <laouarmouaad@gmail.com>
2023-07-20 10:03:48 +0100

    Updat Hooks

--- src/screens/Home/Home.Component.tsx
+++ src/screens/Home/Home.Component.tsx
@@ -69,7 +69,7 @@ const Home = () => {
                                 </MenuItem>
                                 <MenuItem value="resolved">
                                     resolved - {ResolvedSum}
-                                </MenuItem> 
+                                </MenuItem>
                             </Select>
                         </FormControl>
                         <TextField />
